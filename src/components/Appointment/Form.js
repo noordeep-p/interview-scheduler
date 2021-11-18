4cb1d3645da9889feaba40c2d115af910baddd72
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-11-17 21:52:39 -0500

    remove unused function parameters

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,7 +14,7 @@ export default function Form(props) {
 
   const cancel = function (student, interviewer) {
     reset();
-    props.onCancel(student, interviewer);
+    props.onCancel();
   };
 
   return (
@@ -38,7 +38,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={() => cancel(student, interviewer)}>
+          <Button danger onClick={() => cancel()}>
             Cancel
           </Button>
           <Button confirm onClick={() => props.onSave(student, interviewer)}>
