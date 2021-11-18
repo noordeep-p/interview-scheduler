5d0feb48ab0c22311b763bcc7a6ca95a70fa4bc8
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-11-17 21:56:21 -0500

    create custom hook to track visual state of appointment component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (!replace) {
+      setMode(mode);
+      return setHistory([...history, mode]);
+    }
+    return setMode(mode);
+  }
+  function back() {
+    if (history.length === 1) {
+      return setMode(initial);
+    }
+    setMode(history[history.length - 2]);
+    setHistory(history.slice(0, -1));
+  }
+
+  return { mode, transition, back };
+}
