29eaa46ed3a91202b95d6db917583f43c070e28f
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-12-03 15:38:10 -0500

    install propTypes library to enable strict typing & add propTypes to interviewer list componenet to validate the type of the passed prop is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
