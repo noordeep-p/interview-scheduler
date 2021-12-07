7a1701a23052cd69b9b864d6b51fc783ccaf7dd7
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-12-07 18:03:45 -0500

    add cypress test to check if app edits an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -18,4 +18,16 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Noordeep Punjabi");
+    cy.get('[alt="Tori Malcolm"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Noordeep Punjabi");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
